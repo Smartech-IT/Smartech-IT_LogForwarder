5d80e0446bfd6820d39f662e676ab786a4e0294d
Smartech-IT.eu <147044456+Smartech-IT@users.noreply.github.com>
2023-10-05 12:00:04 +0200

    Update Make_me_logforwarder.sh

--- Make_me_logforwarder.sh
+++ Make_me_logforwarder.sh
@@ -7,9 +7,6 @@ stty size | perl -ale 'print "-"x$F[1]'
 echo "Preparing Environment..."
 stty size | perl -ale 'print "-"x$F[1]'
 
-read -p "Enter Host Name: " LogForwarderHostName
-hostname $LogForwarderHostName
-
 mkdir /var/log/remote_logs
 mkdir /etc/nixos/WazuhDocker
 
