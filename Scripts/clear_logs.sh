f29554fd7549a89137b4e1e0335978c90b16b73f
Smartech-IT.eu <147044456+Smartech-IT@users.noreply.github.com>
2024-01-22 10:16:15 +0100

    Update clear_logs.sh

--- Scripts/clear_logs.sh
+++ Scripts/clear_logs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 for file in /var/log/remote_logs/*.log; do : > $file; done
-
+for file in /var/log/warn; do : > $file; done
+for file in /var/log/messages; do : > $file; done
 
